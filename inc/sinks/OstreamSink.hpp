b481975b585803f051335823057af30d887cd5d4
nbaev <nikita_uray@mail.ru>
2025-08-29 18:04:37 +0300

    add mutex to OstreamSink

--- inc/sinks/OstreamSink.hpp
+++ inc/sinks/OstreamSink.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "core/sink.hpp"
+#include <mutex>
 #include <functional>
 #include "utils/formatter.hpp"
 namespace lwslog {
@@ -7,12 +8,14 @@ class OstreamSink : public ILogSink {
 private:
     std::ostream& stream_;
     std::function<std::string(const Message&)> formatter_;
+    std::mutex mtx_;
 public:
     explicit OstreamSink(std::ostream& stream,
                          std::function<std::string(const Message&)> formatter = Formatter::defaultFormatter)
         : stream_(stream), formatter_(std::move(formatter)) {}
 
     void Write(const Messages& msgs) override {
+        std::lock_guard<std::mutex> lock(mtx_);
         for (const auto & msg : msgs) {
             stream_ << formatter_(msg) << std::endl;
         }
